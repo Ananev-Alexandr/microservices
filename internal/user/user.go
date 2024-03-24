5224b28fdfabad8660bcddcb77dca14c445fbce2
Alexandr <ananevalexandr@mail.ru>
2024-03-24 16:16:42 +0300

    Added get+update

--- internal/user/user.go
+++ internal/user/user.go
@@ -2,14 +2,18 @@ package user
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"log"
+	"time"
 
+	auth "github.com/Ananev-Alexandr/microservices/internal/auth"
 	desc_user "github.com/Ananev-Alexandr/microservices/pkg/auth_api"
 	sq "github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"golang.org/x/crypto/bcrypt"
+
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Service struct {
@@ -22,19 +26,55 @@ func NewService(db *pgxpool.Pool) *Service {
 }
 
 func (s *Service) Get(ctx context.Context, req *desc_user.GetRequest) (*desc_user.GetResponse, error) {
-	return nil, errors.New("not implemented")
+	builderSelectOne := sq.Select("id", "name", "email", "role", "created_at", "updated_at").
+		From("user_").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"id": req.Id})
+
+	query, args, err := builderSelectOne.ToSql()
+	if err != nil {
+		log.Printf("failed to build select query: %v", err)
+		return nil, err
+	}
+
+	var (
+		id          int64
+		name, email string
+		role        int32
+		createdAt   time.Time
+		updatedAt   sql.NullTime
+	)
+
+	err = s.db.QueryRow(ctx, query, args...).Scan(&id, &name, &email, &role, &createdAt, &updatedAt)
+	if err != nil {
+		log.Printf("failed to select user: %v", err)
+		return nil, err
+	}
+
+	user := desc_user.GetResponse{
+		Id:        id,
+		Name:      name,
+		Email:     email,
+		Role:      desc_user.Role(role),
+		CreatedAt: timestamppb.New(createdAt),
+	}
+
+	if updatedAt.Valid {
+		user.UpdatedAt = timestamppb.New(updatedAt.Time)
+	}
+
+	return &user, nil
 }
 
 func (s *Service) Create(ctx context.Context, req *desc_user.CreateRequest) (*desc_user.CreateResponse, error) {
-	// log.Printf("Create called with: %v", req)
 	if req.Name == "" || req.Email == "" {
 		return nil, errors.New("have empty field")
 	}
 
-	if InvalidPass(req.Password, req.PasswordConfirm) {
+	if auth.InvalidPass(req.Password, req.PasswordConfirm) {
 		return nil, errors.New("other password")
 	}
-	hash_password, err := HashPassword(req.Password)
+	hash_password, err := auth.HashPassword(req.Password)
 	if err != nil {
 		return nil, errors.New("cannot hash password")
 	}
@@ -63,11 +103,32 @@ func (s *Service) Create(ctx context.Context, req *desc_user.CreateRequest) (*de
 }
 
 func (s *Service) Update(ctx context.Context, req *desc_user.UpdateRequest) (*emptypb.Empty, error) {
+	builderUpdateUser := sq.Update("user_").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"id": req.Id})
+
+	if req.Name != nil && req.Name.Value != "" {
+		builderUpdateUser = builderUpdateUser.Set("name", req.Name.Value)
+	}
+	if req.Email != nil && req.Email.Value != "" {
+		builderUpdateUser = builderUpdateUser.Set("email", req.Email.Value)
+	}
+
+	builderUpdateUser = builderUpdateUser.Set("updated_at", time.Now())
+
+	query, args, err := builderUpdateUser.ToSql()
+	if err != nil {
+		log.Fatalf("failed to build query: %v", err)
+	}
+	_, err = s.db.Exec(ctx, query, args...)
+	if err != nil {
+		log.Printf("failed to execute update query: %v", err)
+		return nil, err
+	}
 	return &emptypb.Empty{}, nil
 }
 
 func (s *Service) Delete(ctx context.Context, req *desc_user.DeleteRequest) (*emptypb.Empty, error) {
-	// log.Printf("Delete called with: %v", req)
 	builderInsert := sq.Delete("user_").
 		PlaceholderFormat(sq.Dollar).
 		Where(sq.Eq{"id": req.Id})
@@ -82,21 +143,5 @@ func (s *Service) Delete(ctx context.Context, req *desc_user.DeleteRequest) (*em
 		log.Printf("failed to delete user: %v", err)
 		return nil, err
 	}
-
-	// Возвращаем пустой ответ после успешного удаления
 	return &emptypb.Empty{}, nil
-}
-
-func InvalidPass(password, password_confirm string) bool {
-	return password != password_confirm
-}
-
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
-}
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
 }
